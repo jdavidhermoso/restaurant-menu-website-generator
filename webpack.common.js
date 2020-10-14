2f41a6620797cb8ea44cb1afd7e2fc2bc7f230a7
David Hermoso <juandavidhermoso@gmail.com>
2020-10-14 08:35:09 +0200

    Add partials and prepare to print. Add QR page

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,7 @@ const HandlebarsPlugin = require("handlebars-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const buildHash = new Date().getTime();
 const restaurantData = require("./restaurant.json");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: { app: "./src/js/main.js" },
@@ -32,25 +33,21 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: `[name].${buildHash}.css`
         }),
         new CopyPlugin([
-            { from: 'assets/favicon.ico', to: '' }
+            { from: 'assets/favicon.ico', to: '' },
+            { from: 'src/qr-generator/qr.png', to: 'assets/qr.png' },
         ]),
-        // new HtmlWebpackPlugin({
-        //     template: './src/index.hbs',
-        // }),
         new HandlebarsPlugin({
             // path to hbs entry file(s). Also supports nested directories if write path.join(process.cwd(), "app", "src", "**", "*.hbs"),
             entry: path.join(process.cwd(), "src", "*.hbs"),
             // output path and filename(s). This should lie within the webpacks output-folder
             // if ommited, the input filepath stripped of its extension will be used
             output: path.join(process.cwd(), "dist", "[name].html"),
-            // you can als add a [path] variable, which will emit the files with their relative path, like
-            // output: path.join(process.cwd(), "build", [path], "[name].html"),
-
-            // data passed to main hbs template: `main - template(data)`
+            partials: [path.join(process.cwd(), 'src', 'components', '**', '*.hbs')],
             data: {
                 buildHash,
                 restaurant: restaurantData,
